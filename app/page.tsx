26cee76e3bcbab83b7ae325d2876b5127e891b8d
kosette <35268640+Kosette@users.noreply.github.com>
2023-04-13 15:36:19 +0800

    fix: minor fix.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import { Home } from "./components/home";
 
-import { getServerSideConfig } from "./config/server";
-
-const serverConfig = getServerSideConfig();
 
 export default async function App() {
   return (
     <>
       <Home />
-      {serverConfig?.isVercel && <Analytics />}
     </>
   );
 }

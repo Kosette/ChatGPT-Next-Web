f47bcfeadeea96dda0e3c5ad9f0e61ab47cc0be1
kosette <35268640+Kosette@users.noreply.github.com>
2023-04-28 07:40:41 +0800

    chore: fix sidebar title and desc.

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,9 +96,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>HAL 2000</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Back to the FUTURE.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />

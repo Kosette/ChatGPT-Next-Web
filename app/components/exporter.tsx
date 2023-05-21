a424d944a169d95cc36c4a030935bb79ef12e643
kosette <35268640+Kosette@users.noreply.github.com>
2023-05-22 02:21:25 +0800

    chore: fix title

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -323,9 +323,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>HAL 2000</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              Fly me to the Moon.
             </div>
             <div className={styles["icons"]}>
               <Avatar avatar={config.avatar}></Avatar>
